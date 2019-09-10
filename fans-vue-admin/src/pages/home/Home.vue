c6f4592f9bf86d22ff0dda5810e24dbf3a18f19b
fanguojie <fanguojie@sxjicheng.com>
2019-09-10 08:15:12 +0800

    [add]header

--- fans-vue-admin/src/pages/home/Home.vue
+++ fans-vue-admin/src/pages/home/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    home
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import HomeHeader from './components/Header'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ fans-vue-admin/src/pages/home/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">輸入城市/景點/</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .header
+        line-height: .86rem
+        background: #00bcd4
+        display: flex
+        color: #fff
+        .header-left
+            width: .64rem
+            float: left
+        .header-input
+            height .64rem
+            line-height .64rem
+            background: #ffffff
+            margin-top: .12rem
+            margin-left:.2rem
+            border-radius: .1rem
+            color: #cccccc
+            flex: 1
+        .header-right
+            width: 1.24rem
+            float: right
+            text-align: center
+</style>
